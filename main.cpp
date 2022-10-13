d6882fdebfe741018d2928e484dd7663ad37dadd
Thibault Cimic <thibaultcimic@outlook.fr>
2022-10-13 09:11:28 +0200

    The mesh class constructor works. Now I'm gonna add the FileManagerC++ as a submodule to use it to split string according to some separator

---
+++ main.cpp
@@ -0,0 +1,21 @@
+// Some includes from the Standard C++ library
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+// Other includes
+#include "mesh.hpp"
+
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+
+  char *filename = "/home/tcimic/Projects/GMSHmesh/mesh/square.msh";
+
+  mesh Th(filename);
+  
+  return 0;
+}
