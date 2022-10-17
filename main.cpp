179873de935f4c16323c495d73efc5c79b6ddabd
Thibault Cimic <thibaultcimic@outlook.fr>
2022-10-17 12:02:15 +0200

    Emptyied the main.cpp

--- main.cpp
+++ main.cpp
@@ -14,15 +14,5 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 
-  char filename[100] = "/home/tcimic/Projects/GMSHmesh/mesh/pipeline_step.msh";
-
-  mesh Th(filename);
-
-  cout << Th.NbCorners() << endl;
-  
-  for (int i0 = 0; i0 < Th.NbEdgesOnBorder(); ++i0) {
-    cout << "e" << i0 << " : " << Th.EdgesOnBorder()[3*i0] << " " << Th.EdgesOnBorder()[3*i0+1] << " " << Th.EdgesOnBorder()[3*i0+2] << endl;
-  }
-  
   return 0;
 }
